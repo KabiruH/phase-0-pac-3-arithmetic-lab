9571512f48c71889489bab665682c432a248d9cb
KabiruH <kelkab46@gmail.com>
2022-11-22 02:07:19 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add (x,y){
+    return (x+y)
+}
+function subtract (x,y){
+    return (x-y)
+}
+function multiply (x,y){
+    return (x*y)
+}
+function divide (x,y){
+    return (x/y)
+}
+function increment (y){
+    return (y +=1)
+}
+function decrement (y){
+    return (y -=1)
+}
+function makeInt (y){
+    return parseInt (y, 10)
+}
+function preserveDecimal (y){
+    return parseFloat (y)
+}
